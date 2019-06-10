8d0740090fb83606e59325e6af595d9ddaa0b504
David Reina <u1201730@unimilitar.edu.co>
2019-06-09 22:38:56 -0500

    Pa paaaaaaa

--- models/vacante.js
+++ models/vacante.js
@@ -2,13 +2,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var VacanateSchema = new Schema({
+    id_empleador: Schema.Types.ObjectId,
     Cargo: String,
     Fecha_pub: String,
     Fecha_tar: String,
     Lugar: String,
     Tiempo: Number,
-    pago: Number
-}, {collection: 'vacantes'});
+    pago: Number,
+    num_vacantes: Number,
+    express: Boolean,
+    postulados: [{
+        id: Schema.Types.ObjectId,
+        nombre: String,
+        aceptado: Boolean
+    }]
+}, { collection: 'vacantes' });
 
 //model
 module.exports = mongoose.model('Vacante', VacanateSchema);
